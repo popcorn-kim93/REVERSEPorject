5ad7c2a21db09300798ed4db6a9d461ae1147e1c
popcorn-kim93 <chakana2003@gmail.com>
2022-12-30 10:58:56 +0900

    컨벤션 수정

--- Source/ReverseProj/C_S_CurrentStatus.h
+++ Source/ReverseProj/C_S_CurrentStatus.h
@@ -6,27 +6,29 @@
 #include "C_S_CurrentStatus.generated.h"
 
 USTRUCT(BlueprintType)
-struct FC_S_Position {
+struct FC_S_Position
+{
 	GENERATED_USTRUCT_BODY()
 
 		FC_S_Position(FVector NewLocation = FVector(0.f,0.f,0.f), FQuat NewRotation = FQuat(0.f,0.f,0.f,0.f)) : Location(NewLocation), Rotation(NewRotation) {}
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite)
 	FVector Location;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite)
 	FQuat Rotation;
 };
 
 USTRUCT(BlueprintType)
-struct FC_S_Velocity {
+struct FC_S_Velocity
+{
 	GENERATED_USTRUCT_BODY()
 
 		FC_S_Velocity(FVector NewDegree = FVector(0.f,0.f,0.f), FVector NewScale=FVector(0.f,0.f,0.f)) : Degree(NewDegree), Scale(NewScale) {}
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite)
 	FVector Degree;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UPROPERTY(BlueprintReadWrite)
 	FVector Scale;
 };
--- Source/ReverseProj/MainMenu/C_MainMenuPC.cpp
+++ Source/ReverseProj/MainMenu/C_MainMenuPC.cpp
@@ -4,38 +4,46 @@
 #include "Kismet/GameplayStatics.h"
 #include "TimerManager.h"
 
-void AC_MainMenuPC::BeginPlay() {
+void AC_MainMenuPC::BeginPlay()
+{
 	Super::BeginPlay();
 
 	TArray<AActor*> Outer;
 	UGameplayStatics::GetAllActorsWithTag(GetWorld(), TEXT("MainMenuCam"), Outer);
 
-	if (Outer.IsValidIndex(0)) {
+	if (Outer.IsValidIndex(0))
+	{
 		SetViewTarget(Outer[0]);
 	}
-	else {
+	else
+	{
 		UE_LOG(LogClass, Warning, TEXT("MainMenuCam is Not Exist. Fatal Error!!!"));
 	}
 
 	GI = Cast<UC_ReverseGI>(GetGameInstance());
-	if (!(GI->IsValidLowLevel())) {
+	if (!(GI->IsValidLowLevel()))
+	{
 		UE_LOG(LogClass, Warning, TEXT("GI Cast Error. In MainMenuPC"));
 	}
 
 	GetWorldTimerManager().SetTimer(TimeReverse, this, &AC_MainMenuPC::DoReverse, 10.f, false);
 }
 
-void AC_MainMenuPC::DoReverse() {
+void AC_MainMenuPC::DoReverse()
+{
 	GetWorldTimerManager().ClearTimer(TimeReverse);
-	if (GI) {
+	if (GI)
+	{
 		GI->ReverseActivate();
 	}
 	GetWorldTimerManager().SetTimer(TimeReverse, this, &AC_MainMenuPC::DoUnReverse, 12.f, false);
 }
 
-void AC_MainMenuPC::DoUnReverse() {
+void AC_MainMenuPC::DoUnReverse()
+{
 	GetWorldTimerManager().ClearTimer(TimeReverse);
-	if (GI) {
+	if (GI) 
+	{
 		GI->ReverseDeActivate();
 	}
 	GetWorldTimerManager().SetTimer(TimeReverse, this, &AC_MainMenuPC::DoReverse, 10.f, false);
